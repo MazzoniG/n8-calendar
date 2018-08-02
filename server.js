c8c5b00d54f7b21f5fde3fb55fac4d9d56a6db57
mazzonig <mazzoni2092@gmail.com>
2018-08-02 17:46:49 -0600

    ExpressJS Up | Holidays endpoint ready

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express'), app = express();
+var chalk = require('chalk');
+var holidays = require('date-holidays');
+
+var port = process.env.PORT || 3000;
+
+//Countries' holidays | Endpoint (/holidays/US)
+app.get('/holidays/:country', function(req, res) {
+	var hd = new holidays(req.params.country.toUpperCase());
+	res.json(hd.getHolidays());
+});
+
+app.listen(port);
+
+console.log(chalk.green('Server listening  on port ' + port));
+
+exports = module.exports = app;
