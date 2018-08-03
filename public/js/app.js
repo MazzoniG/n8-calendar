fc9f9f272e118ba0a8b0b3529426111c163bedd2
mazzonig <mazzoni2092@gmail.com>
2018-08-02 19:12:54 -0600

    Calendar implemented

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('number8-calendar', ['ngRoute', 'appRoutes', 'ngMaterial', 'HomeCtrl'])
+angular.module('number8-calendar', ['ngRoute', 'appRoutes', 'ngMaterial', 'ui.bootstrap', 'ui.calendar', 'angularMoment', 'HomeCtrl'])
 
 .config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
